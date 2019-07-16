7f60a1e72a6ef0bd13182a9e9ce096199b38cf88
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2019-07-16 16:18:40 +0900

    Attention Seq2Seq 모델 완성

---
+++ 02_Seq2SeqAttention/model.py
@@ -0,0 +1,186 @@
+# -*- coding:utf-8 -*-
+import torch
+import torch.nn as nn
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, embedding, seq_len, rnn_dim, n_layer, dropout_rate=0):
+        super().__init__()
+        self.embedding = embedding
+        self.seq_len = seq_len
+        self.rnn_dim = rnn_dim
+        self.n_layer = n_layer
+
+        embedding_dim = embedding.embedding_dim
+        self.batch_norm = nn.BatchNorm1d(seq_len)
+        self.rnn = nn.LSTM(embedding_dim, rnn_dim, n_layer, batch_first=True, dropout=dropout_rate)
+
+    def forward(self, src_input, src_length):
+        embedded = self.embedding(src_input)
+        embedded = self.batch_norm(embedded)
+        packed = nn.utils.rnn.pack_padded_sequence(embedded, src_length, batch_first=True)
+        outputs, (hidden, cell) = self.rnn(packed)
+        outputs, outputs_length = nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True)
+        # outputs => (batch_size, seq_len, rnn_dims)
+        del outputs_length
+
+        return outputs, hidden, cell
+
+
+class DecoderAttentionRNN(nn.Module):
+    def __init__(self, attention, embedding, rnn_dim, out_dim, n_layer=1, dropout_rate=0):
+        super().__init__()
+        self.attention = attention
+        self.embedding = embedding
+        self.out_dim = out_dim
+
+        embedding_dim = embedding.embedding_dim
+        self.batch_norm = nn.BatchNorm1d(embedding_dim)
+        self.rnn = nn.LSTM(embedding_dim, rnn_dim, n_layer, dropout=dropout_rate, batch_first=True)
+        self.linear = nn.Linear(rnn_dim * 2, out_dim)
+
+    def forward(self, enc_outputs, dec_input, last_hidden, last_cell):
+        # enc_outputs => (batch_size, seq_len, rnn_dim)
+        # dec_input => (batch_size)
+        embedded = self.embedding(dec_input)        # => (batch_size, embedding_size)
+        embedded = self.batch_norm(embedded)        # => (batch_size, embedding_size)
+        embedded = embedded.unsqueeze(1)            # => (batch_size, 1, embedding_size)
+
+        dec_output, (dec_hidden, dec_cell) = self.rnn(embedded)  # => (batch_size, 1, rnn_dim)
+
+        # calc attention distribution
+        attention_distribution = self.attention(enc_outputs, dec_output)    # => (batch_size, seq_len, 1)
+
+        # calc attention value (= context vector)
+        temp = enc_outputs * attention_distribution                         # => (batch_size, seq_len, rnn_dim)
+        context_vector = temp.sum(dim=1)                                    # => (batch_size, rnn_dim)
+
+        # concat context vector
+        dec_output = dec_output.squeeze(dim=1)                              # => (batch_size, rnn_dim)
+        concat = torch.cat((dec_output, context_vector), dim=1)             # => (batch_size, rnn_dim * 2)
+        predication = self.linear(concat)                                   # => (batch_size, out_dim)
+        return predication, dec_hidden, dec_cell
+
+
+class Seq2SeqAttention(nn.Module):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, src_input, src_length, tar_input):
+        enc_output, hidden, cell = self.encoder(src_input, src_length)
+
+        batch_size, max_len = tar_input.shape
+        out_dim = self.decoder.out_dim
+        outputs = torch.zeros((batch_size, max_len, out_dim))
+
+        input_ = tar_input[:, 0]
+
+        for t in range(1, max_len):
+            predication, hidden, cell = self.decoder(enc_output, input_, hidden, cell)
+            outputs[:, t] = predication
+            values, indices = predication.max(dim=1)
+            del values
+            input_ = indices
+
+        return outputs
+
+
+class Attention(nn.Module):
+    """
+    for which we consider three different alternatives:
+
+    Score function
+
+    score(ht, hs) =
+        htT * hS                =>  (dot)
+        htT * W * hs            =>  (general)
+        vT * tanh * (W*[ht;hs]) =>  (concat)
+    """
+
+    def __init__(self, method, rnn_dim=None):
+        super().__init__()
+        self.method = method
+        if self.method not in ['dot', 'general', 'concat']:
+            raise NotImplementedError('implement [dot, general, concat]')
+
+        if self.method == 'general':
+            self.nn = nn.Linear(rnn_dim, rnn_dim)
+        if self.method == 'concat':
+            self.nn = nn.Linear(rnn_dim * 2, rnn_dim)
+            self.v = nn.Linear(rnn_dim, 1)
+
+    def dot(self, src_inputs, tar_input):
+        """
+            src_inputs => (batch_size, seq_len, rnn_dim)
+            tar_input => (batch_size, rnn_dim, 1)
+
+            < dot product >
+
+            => src_input * tar_input
+            => (batch_size, seq_len, rnn_dim) * (batch_size, rnn_dim, 1) = (batch_size, seq_len, 1)
+        """
+        attention_value = src_inputs.bmm(tar_input)      # (batch_size, seq_len, 1)
+        return attention_value
+
+    def general(self, src_inputs, tar_input):
+        """
+            src_inputs => (batch_size, seq_len, rnn_dim)
+            tar_input => (batch_size, rnn_dim, 1)
+            weight => (rnn_dim, rnn_dim)
+
+            < general >
+
+            => src_input x weight x tar_input
+            => (batch_size, seq_len, rnn_dim) x (rnn_dim, rnn_dim) x (batch_size, rnn_dim, 1) = (batch_size, seq_len, 1)
+        """
+        attention_value = self.nn(src_inputs)
+        attention_value = attention_value.bmm(tar_input)    # => (batch_size, seq_len, 1)
+        return attention_value
+
+    def concat(self, src_inputs, tar_input):
+        """
+            src_inputs => (batch_size, seq_len, rnn_dim)
+            tar_input => (batch_size, rnn_dim, 1)
+        """
+        src_inputs = src_inputs.permute(0, 2, 1)                    # => (batch_size, rnn_dim, seq_len)
+        tar_input = tar_input.expand(-1, -1, src_inputs.size(2))    # => (batch_size, rnn_dim, seq_len)
+        hidden = torch.cat((src_inputs, tar_input), dim=1)          # => (batch_size, rnn_dim * 2, seq_len)
+
+        # 1. hidden
+        #       => (batch_size, rnn_dim * 2, seq_len)
+        #
+        # 2. hidden permute
+        #       => (batch_size, seq_len, rnn_dim * 2)
+        #
+        # 3. hidden x weight
+        #       => (batch_size, seq_len, rnn_dim * 2) * (rnn_dim * 2, rnn_dim) = (batch_size, seq_len, rnn_dim)
+        #
+        # 4. tanh
+        #       => (batch_size, seq_len, rnn_dim)
+        #
+        # 5. v * hidden
+        #       => (batch_size, seq_len, rnn_dim) * (rnn_dim, 1) = (batch_size, seq_len, 1)
+
+        attention_value = self.v(torch.tan(self.nn(hidden.permute(0, 2, 1))))
+        return attention_value
+
+    def forward(self, src_inputs, tar_input):
+        # src_input => (batch_size, seq_len, rnn_dim)
+        # tar_input => (batch_size, 1, rnn_dim)
+
+        # target transpose
+        tar_input = tar_input.permute(0, 2, 1)                          # => (batch_size, rnn_dim, 1)
+
+        attention_value = None
+        if self.method == 'dot':
+            attention_value = self.dot(src_inputs, tar_input)
+        elif self.method == 'general':
+            attention_value = self.general(src_inputs, tar_input)
+        elif self.method == 'concat':
+            attention_value = self.concat(src_inputs, tar_input)
+
+        # attention_value => (batch_size, seq_len, 1)
+        attention_distribution = nn.Softmax(dim=1)(attention_value)     # => (batch_size, seq_len, 1)
+        return attention_distribution
---
+++ 02_Seq2SeqAttention/train.py
@@ -0,0 +1,182 @@
+# -*- coding:utf-8 -*-
+import os
+import time
+import torch
+import argparse
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from data_helper import TranslationDataset, create_or_get_voc, create_or_get_word2vec, apply_word2vec_embedding_matrix
+from model import EncoderRNN, Attention, DecoderAttentionRNN, Seq2SeqAttention
+from tensorboardX import SummaryWriter
+
+
+writer = SummaryWriter()
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', default='../Dataset', type=str)
+    parser.add_argument('--word2vec_path', default='../Word2Vec', type=str)
+    parser.add_argument('--rnn_sequence_size', default=20, type=int)
+    parser.add_argument('--min_count', default=0, type=int)
+    parser.add_argument('--max_count', default=8000, type=int)
+    parser.add_argument('--embedding_size', default=200, type=int)
+    parser.add_argument('--rnn_dim', default=50, type=int)
+    parser.add_argument('--rnn_layer', default=1, type=int)
+    parser.add_argument('--attention_method', default='concat', type=str)
+    parser.add_argument('--batch_size', default=512, type=int)
+    parser.add_argument('--epochs', default=500, type=int)
+    parser.add_argument('--model_path', default='./save_model/0_seq2seq.pth', type=str)
+    args = parser.parse_args()
+    return args
+
+
+def init_weights(m):
+    for name, param in m.named_parameters():
+        nn.init.uniform_(param.data, -0.08, 0.08)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def calculation_loss(out, tar, loss_func):
+    out = out.view(-1, out.size(-1))        # => (batch_size * seq_len, voc_size)
+    tar = tar.view(-1)                      # => (batch_size * seq_len)
+
+    out = out.to(device)
+    tar = tar.to(device)
+
+    loss_ = loss_func(out, tar)
+    return loss_
+
+
+def train():
+    args = get_args()
+    x_train_path = os.path.join(args.data_path, 'train.ko')
+    y_train_path = os.path.join(args.data_path, 'train.en')
+    x_dev_path = os.path.join(args.data_path, 'dev.ko')
+    y_dev_path = os.path.join(args.data_path, 'dev.en')
+
+    # create or load vocabulary
+    ko_voc, en_voc = create_or_get_voc(
+        x_train_path,
+        y_train_path,
+        save_path='../Dictionary',
+        min_count=args.min_count,
+        max_count=args.max_count
+    )
+    ko_word_len = len(ko_voc.word2idx)
+    en_word_len = len(en_voc.word2idx)
+
+    # create or load word2vec model
+    ko_word2vec, en_word2vec = create_or_get_word2vec(args.word2vec_path, x_train_path, y_train_path)
+
+    # embedding matrix
+    ko_embedding = nn.Embedding(ko_word_len, args.embedding_size)
+    en_embedding = nn.Embedding(en_word_len, args.embedding_size)
+
+    # define model
+    encoder = EncoderRNN(ko_embedding, args.rnn_sequence_size, args.rnn_dim, args.rnn_layer)
+    attention = Attention(args.attention_method, args.rnn_dim)
+    decoder = DecoderAttentionRNN(attention, en_embedding, args.rnn_dim, en_word_len, args.rnn_layer)
+    model = Seq2SeqAttention(encoder, decoder)
+    model.train()
+    model.to(device)
+
+    # initialize weight
+    model.apply(init_weights)
+
+    # initialize embedding matrix - apply word2vec embedding matrix
+    ko_embedding = apply_word2vec_embedding_matrix(ko_word2vec, ko_embedding, ko_voc)
+    en_embedding = apply_word2vec_embedding_matrix(en_word2vec, en_embedding, en_voc)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    criterion = nn.CrossEntropyLoss(ignore_index=en_voc.word2idx[en_voc.PAD])
+
+    # if exist model => load model
+    if os.path.exists(args.model_path):
+        print(f'load model : {args.model_path}')
+        checkpoint = torch.load(args.model_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    # load train data & loader
+    train_data = TranslationDataset(x_train_path, y_train_path, ko_voc, en_voc, args.rnn_sequence_size)
+    train_loader = DataLoader(train_data, shuffle=True, batch_size=20)
+
+    # load dev data & loader
+    dev_data = TranslationDataset(x_dev_path, y_dev_path, ko_voc, en_voc, args.rnn_sequence_size)
+    dev_loader = DataLoader(dev_data, shuffle=True, batch_size=int(dev_data.__len__() / 50))
+
+    # Training
+    start_time = time.time()
+    global_step = 0
+    for epoch in range(args.epochs):
+        epoch_avg_train_loss = 0
+
+        for i, data in enumerate(train_loader, 0):
+            # train_enc_input  => (batch_size, sequence_size)
+            # train_enc_length => (batch_size)
+            # train_dec_input  => (batch_size, sequence_size)
+            # train_dec_output => (batch_size, sequence_size)
+            train_enc_input, train_enc_length, train_dec_input, train_dec_output = data
+            train_enc_length, sorted_idx = train_enc_length.sort(0, descending=True)
+            train_enc_input = train_enc_input[sorted_idx]
+
+            # nn forward
+            output = model(train_enc_input, train_enc_length, train_dec_input)
+
+            # get loss
+            loss = calculation_loss(output, train_dec_output, criterion)
+            writer.add_scalar('loss', loss.item(), global_step)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Dev data loss calculation
+            if i % 50 == 0 and i != 0:
+                dev_avg_loss = 0
+                count = 0
+                for dev_enc_input, dev_enc_length, dev_dec_input, dev_dec_output in dev_loader:
+                    dev_enc_length, sorted_idx = dev_enc_length.sort(0, descending=True)
+                    dev_enc_input = dev_enc_input[sorted_idx]
+
+                    dev_output = model(dev_enc_input, dev_enc_length, dev_dec_input)
+                    dev_loss = calculation_loss(dev_output, dev_dec_output, criterion)
+                    dev_avg_loss += dev_loss.item()
+                    count += 1
+
+                dev_avg_loss = dev_avg_loss / count
+                print('epoch : {0:2d} iter : {1:4d}  =>  train_loss : {2:4f}  dev_loss : {3:4f}'.format(
+                    epoch, i, loss.item(), dev_avg_loss))
+            else:
+                epoch_avg_train_loss += loss.item()
+                print('epoch : {0:2d} iter : {1:4d}  =>  train_loss : {2:4f}'.format(epoch, i, loss.item()))
+
+            global_step += 1
+
+        # save model
+        if epoch % 5 == 0:
+            base_path = os.path.dirname(args.model_path)
+            full_path = os.path.join(base_path, str(epoch)+'_attention_seq2seq.pth')
+            torch.save({
+                'epoch': epoch+1,
+                'ko_embedding': ko_embedding.state_dict(),
+                'en_embedding': en_embedding.state_dict(),
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': epoch_avg_train_loss / len(iter(train_loader))
+            }, full_path)
+
+    end_time = time.time()
+    writer.close()
+
+    print('Training ending... !! time : {}'.format(end_time - start_time))
+
+
+if __name__ == '__main__':
+    train()
